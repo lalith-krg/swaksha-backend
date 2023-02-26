db219af34031b613b9450518d6d8a02335ff1a92
Shrey Tripathi <tripathi@Tripathi-MacBook-Pro.local>
2023-02-26 21:53:37 +0530

    new code

--- gateway-service/src/main/java/com/swaksha/gatewayservice/authentication/AuthenticationService.java
+++ gateway-service/src/main/java/com/swaksha/gatewayservice/authentication/AuthenticationService.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
-    private static final String ACCOUNT_SID = "ACbc91ba0b1ce5b5020130385133c2ae46"; //System.getenv("ACbc91ba0b1ce5b5020130385133c2ae46");
-    private static final String AUTH_TOKEN = "aa2cab81ac3f795ce2280dbe04659ef1"; //System.getenv("aa2cab81ac3f795ce2280dbe04659ef1");
+    private static final String ACCOUNT_SID = "AC6768d08fe263d31bfd6f4b021dfe9749"; //System.getenv("ACbc91ba0b1ce5b5020130385133c2ae46");
+    private static final String AUTH_TOKEN = "e46d14f979d967de9a8db986135c4c33"; //System.getenv("aa2cab81ac3f795ce2280dbe04659ef1");
     public void create_service() {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         com.twilio.rest.verify.v2.Service service = com.twilio.rest.verify.v2.Service.creator("My First Verify Service").create();
@@ -19,7 +19,7 @@ public class AuthenticationService {
     public String send_otp(String mobile_number) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Verification verification = Verification.creator(
-                        "VAe7a8961bee78f7920d14ec2ce79d82c1",
+                        "VAd9a20d283c55b667aaa95a27e9c5d6be",
                         "+91"+mobile_number,
                         "sms")
                 .create();
@@ -30,7 +30,7 @@ public class AuthenticationService {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         VerificationCheck verificationCheck = VerificationCheck.creator(
-                        "VAe7a8961bee78f7920d14ec2ce79d82c1", otp)
+                        "VAd9a20d283c55b667aaa95a27e9c5d6be", otp)
                 .setTo("+91"+mobile_number)
                 .create();
         System.out.println(verificationCheck.getStatus());
