2efe1e5b5736d9503a4ecc1bece70236e253e994
Shrey Tripathi <shrey.tripathi@iiitb.ac.in>
2023-03-29 10:55:25 +0530

    Added backend for admin-service (v1)

---
+++ admin-service/src/test/java/com/swaksha/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swaksha.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
