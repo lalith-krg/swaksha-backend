95a156ab4403fef823962e38f48fe15cc9f2ea9e
Shrey Tripathi <shrey.tripathi@iiitb.ac.in>
2023-03-30 18:42:49 +0530

    Consent request initiate in Hospital (v1), mysql dump

--- hospital-service/src/main/java/com/swaksha/hospitalservice/controller/HospitalController.java
+++ hospital-service/src/main/java/com/swaksha/hospitalservice/controller/HospitalController.java
@@ -1,18 +1,30 @@
 package com.swaksha.hospitalservice.controller;
 
 import com.swaksha.hospitalservice.entity.Patient;
+import com.swaksha.hospitalservice.entity.Hospital;
 import com.swaksha.hospitalservice.service.HospitalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/health/facility")
 public class HospitalController {
+
+    private RestTemplate restTemplate = new RestTemplateBuilder().build();
+
+    record doctorRequestBodyWithoutConsent(String docSSID, String patientSSID){}
+
+//    record doctorRequestBodyWithConsent(String docSSID, String hiuSSID, String patientSSID, ConsentObj consentObj){}
+
 
     @Autowired
     private HospitalService hospitalService;
@@ -104,5 +116,16 @@ public class HospitalController {
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/hospital/request")
+    public void hiuRequest(@RequestBody doctorRequestBodyWithoutConsent doctorRequestBodyWithoutConsent){
+
+        // call /gateway/request/hiu/request
+        String url = "http://localhost:8999/gateway/request/hiu/request";
+
+        HttpEntity<doctorRequestBodyWithoutConsent> consentEntity = new HttpEntity<>(doctorRequestBodyWithoutConsent);
+
+        this.restTemplate.postForEntity(url, consentEntity, Boolean.class);
     }
 }
