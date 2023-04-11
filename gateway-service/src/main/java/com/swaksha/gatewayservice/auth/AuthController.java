baa298ef97463b6cec41f7ee567a632adfcd58fe
GSri30 <sri_3007@hotmail.com>
2023-04-12 04:54:20 +0530

    add: sha256, assign and verify api-key

--- gateway-service/src/main/java/com/swaksha/gatewayservice/auth/AuthController.java
+++ gateway-service/src/main/java/com/swaksha/gatewayservice/auth/AuthController.java
@@ -1,13 +1,16 @@
 package com.swaksha.gatewayservice.auth;
 
 import com.swaksha.gatewayservice.authentication.AuthenticationService;
-import jakarta.servlet.http.HttpServletResponse;
+import com.swaksha.gatewayservice.repository.APIRepo;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.http.HttpResponse;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -17,7 +20,7 @@ public class AuthController {
     private final AuthService service;
     private final AuthenticationService otp_service;
 
-
+    record assignRequest(String ssid){}
 
 /*    public AuthController(AuthService service,AuthenticationService authenticationService)
     {
@@ -50,11 +53,34 @@ public class AuthController {
         System.out.println(request.getSsid());
         return ResponseEntity.ok(service.authenticate(request));
     }
+
+    // @GetMapping("/create-api-key")
+    // public boolean createApiKey(HttpServletRequest request){
+    //     String key = request.getHeader("swaksha-api-key");
+    //     System.out.println(key);
+    //     String api_key = service.createAPIKey();
+    //     String api_key_hashed = AuthService.generateSHA256(api_key);
+    //     boolean is_same = AuthService.verifySHA256(api_key, api_key_hashed);
+    //     System.out.println(api_key);
+    //     System.out.println(api_key_hashed);
+    //     return is_same;
+    // }
+
+    @PostMapping("/assign-api-key")
+    public String createApiKey(@RequestBody assignRequest request){
+        String api_key = service.assignAPIKey(request.ssid);
+        return api_key;
+    }
+
+    @PostMapping("/verify-api-key")
+    public boolean verifyApiKey(@RequestBody assignRequest request, HttpServletRequest http_request){
+        String api_key = http_request.getHeader("swaksha-api-key");
+        boolean valid = service.verifyAPIKey(request.ssid, api_key);
+        return valid;
+    }
+
     @PostMapping("/demo")
     public String sayHello(){
         return "hello from patient endpoint";
     }
-
-
-
 }
